52ca3d8681954ba0483c31d7b57ac683fba8c401
Carl Bateman <carlbateman@hotmail.com>
2019-10-03 16:41:17 +0100

    Corrected comparison

--- vec3.js
+++ vec3.js
@@ -185,7 +185,7 @@ var Vec3 = (function () {
   };
 
   Vec3.prototype.equals = function (v, tolerance) {
-    tolerance = tolerance != null ? tolerance : 0.0000001;
+    tolerance = tolerance !== null ? tolerance : 0.0000001;
     return (Math.abs(v.x - this.x) <= tolerance) && (Math.abs(v.y - this.y) <= tolerance) && (Math.abs(v.z - this.z) <= tolerance);
   };
 
