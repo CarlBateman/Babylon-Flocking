48930670373d03065b9f1dfb08a870c143e6a3e3
Carl Bateman <carlbateman@hotmail.com>
2019-12-07 12:58:13 +0000

    Factored out defaults

--- Flock.js
+++ Flock.js
@@ -1,4 +1,5 @@
 ﻿// relies on vec3.js
+// relies on octree.js
 var FLOCKING = FLOCKING || (function () {
   return {};
 })();
@@ -169,40 +170,32 @@ FLOCKING.Flock = function (numBoids = 0) {
   };
 };
 
-FLOCKING.Boid = function ({ velocity = new Vec3(0, 0, 1),
-  position = new Vec3(10, 0, 0),
-  maxSpeed = 1
-} = {}
-) {
-  let Boid = FLOCKING.Boid;
 
-  if (!(this instanceof Boid)) {
-    return new Boid();
+FLOCKING.Boid = function (options) {
+  if (typeof options !== "object" && typeof options !== "undefined" ) {
+    throw "FLOCKING.Boid options must be an object or undefined";
   }
 
-  this.mass = 1;
-  this.velocity = velocity;
-  this.position = position;
-  this.maxSpeed = maxSpeed;
-  this.maxAcceleration = 0.1;
-  this.numNeighbours = 8;
-  this.separateNeighbourRadius = 8;
-  this.alignNeighbourRadius = 20;
-  this.cohereNeighbourRadius = 10;
-  this.cohereStrength = 1;
-  this.alignStrength = 1;
-  this.separateStrength = 1;
-  this.radius = 1;
-  this.groupId = 0;
-  this.mix = 0;
-  this.wrap = true;
-  this.bound = false;
-  this.hidden = false;
-  this.separateNumberOfNeighbours = 10;
-  this.alignNumberOfNeighbours = 10;
-  this.cohereNumberOfNeighbours = 10;
+  let Boid = FLOCKING.Boid;
+  if (!(this instanceof Boid)) {
+    return new Boid(options);
+  }
 
-  this.heading = new Vec3();
+  let defaults = Boid.getDefaults();
+
+  if (typeof options === "undefined") {
+    for (let [key, value] of Object.entries(defaults)) {
+      this[key] = value;
+    }
+  } else {
+    for (let [key, value] of Object.entries(defaults)) {
+      if (key in options)
+        this[key] = options[key];
+      else
+        this[key] = value;
+    }
+  }
+
   // private
   let acceleration = new Vec3(0, 0, 0);
   let neighboursSame = [];
@@ -487,3 +480,96 @@ FLOCKING.Boid = function ({ velocity = new Vec3(0, 0, 1),
     return idx++;
   };
 }).call(FLOCKING.Boid);
+
+(function () {
+  defaults = {
+    mass: 1,
+    velocity: new Vec3(0, 0, 1),
+    position: new Vec3(0, 0, 0),
+    maxSpeed: 1,
+    maxAcceleration: 0.1,
+    numNeighbours: 8,
+    separateNeighbourRadius: 8,
+    alignNeighbourRadius: 20,
+    cohereNeighbourRadius: 10,
+    cohereStrength: 1,
+    alignStrength: 1,
+    separateStrength: 1,
+    radius: 1,
+    groupId: 0,
+    mix: 0,
+    wrap: true,
+    bound: false,
+    hidden: false,
+    separateNumberOfNeighbours: 10,
+    alignNumberOfNeighbours: 10,
+    cohereNumberOfNeighbours: 10,
+    heading: new Vec3(),
+  };
+
+  this.getDefaults = function () {
+    return clone(defaults);
+  };
+
+  this.addDefault = function (name, value) {
+    defaults[name] = value;
+  };
+
+  this.setDefault = function (name, value) {
+    defaults[name] = value;
+  };
+
+  // taken from https://davidwalsh.name/javascript-clone
+  function clone(src) {
+    function mixin(dest, source, copyFunc) {
+      var name, s, i, empty = {};
+      for (name in source) {
+        // the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+        // inherited from Object.prototype.	 For example, if dest has a custom toString() method,
+        // don't overwrite it with the toString() method that source inherited from Object.prototype
+        s = source[name];
+        if (!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))) {
+          dest[name] = copyFunc ? copyFunc(s) : s;
+        }
+      }
+      return dest;
+    }
+
+    if (!src || typeof src != "object" || Object.prototype.toString.call(src) === "[object Function]") {
+      // null, undefined, any non-object, or function
+      return src;	// anything
+    }
+    if (src.nodeType && "cloneNode" in src) {
+      // DOM Node
+      return src.cloneNode(true); // Node
+    }
+    if (src instanceof Date) {
+      // Date
+      return new Date(src.getTime());	// Date
+    }
+    if (src instanceof RegExp) {
+      // RegExp
+      return new RegExp(src);   // RegExp
+    }
+    var r, i, l;
+    if (src instanceof Array) {
+      // array
+      r = [];
+      for (i = 0, l = src.length; i < l; ++i) {
+        if (i in src) {
+          r.push(clone(src[i]));
+        }
+      }
+      // we don't clone functions for performance reasons
+      //		}else if(d.isFunction(src)){
+      //			// function
+      //			r = function(){ return src.apply(this, arguments); };
+    } else {
+      // generic objects
+      r = src.constructor ? new src.constructor() : {};
+    }
+    return mixin(r, src, clone);
+
+  }
+}).call(FLOCKING.Boid);
+
